c3ae5d0c20095b1cc0315ef3ceace3e80bf13122
zui <626255007@qq.com>
2016-11-06 09:19:04 +0800

    split_part

--- prework.py
+++ prework.py
@@ -13,11 +13,23 @@ def format_string(string):
     return retstring
 
 
-# log去符号
-def get_text(filename, partnum):
-    #if not os.path.exists(filename):
-    #    os.mkdir(filename)
+# log去符号(不分part)
+def get_text(filename):
+    f = open(filename + ".txt", "rb")
+    context = f.readlines()
 
+    wf = open(filename + "/clean" + ".txt", "wb")
+    for line in context:
+        s = format_string(line)
+        if s != "":
+            wf.write(s + "\n")
+    wf.close()
+    f.close()
+    print filename + " get_text  DONE!"
+
+
+# log去符号(分part)
+def get_text_part(filename, partnum):
     for part in xrange(partnum):
         f = open(filename + "/part" + str(part)+ ".txt", "rb")
         context = f.readlines()
@@ -28,22 +40,44 @@ def get_text(filename, partnum):
                 wf.write(s + "\n")
         wf.close()
         f.close()
-        print filename + " part" + str(part) + " get_text  DONE!"
+        print filename + " part" + str(part) + " get_text_part  DONE!"
 
-    print filename + " get_text  DONE!\n"
+    print filename + " get_text_part  DONE!\n"
 
 
-# 获得字典
-def get_dic(filename, partnum):
-    #if not os.path.exists(filename):
-    #    os.mkdir(filename)
+# 获得字典(不分part)
+def get_dic(filename):
+    wf = open(filename + "/dic.txt", "wb")
+    dic = {}
+
+    f = open(filename + "/clean" + ".txt", "rb")
+    context = f.readlines()
+    for line in context:
+        sentence = line.strip().split(" ")
+        for word in sentence:
+            if word in dic:
+                dic[word] += 1
+            else:
+                dic[word] = 1
+    f.close()
+
+    okdic = sorted(dic.items(), key=lambda e: e[1], reverse=True)
+    # print len(okdic)
+    for item in okdic:
+        wf.write("%s,%d\n" % (item[0], item[1]))
+
+    wf.close()
+    print filename + " get_dic  DONE!\n"
+
+
+# 获得字典(分part)
+def get_dic_part(filename, partnum):
     wf = open(filename + "/dic.txt", "wb")
     dic = {}
 
     for part in xrange(partnum):
         f = open(filename + "/clean." + "part" + str(part) + ".txt", "rb")
         context = f.readlines()
-
         for line in context:
             sentence = line.strip().split(" ")
             for word in sentence:
@@ -52,7 +86,7 @@ def get_dic(filename, partnum):
                 else:
                     dic[word] = 1
         f.close()
-        print filename + " part" + str(part) + " get_dic  DONE!"
+        print filename + " part" + str(part) + " get_dic_part  DONE!"
 
     okdic = sorted(dic.items(), key=lambda e: e[1], reverse=True)
     #print len(okdic)
@@ -60,7 +94,7 @@ def get_dic(filename, partnum):
         wf.write("%s,%d\n" % (item[0], item[1]))
 
     wf.close()
-    print filename + " get_dic  DONE!\n"
+    print filename + " get_dic_part  DONE!\n"
 
 
 if __name__ == "__main__":
@@ -69,5 +103,5 @@ if __name__ == "__main__":
              "BaseLine-BigData_1kUE_20ENB_paging-Case_Group_1-Case_1"]
     path = "data/network_diagnosis_data/"
     for name in names:
-        get_text(path + name, 10)
-        get_dic(path + name, 10)
+        get_text_part(path + name, 10)
+        get_dic_part(path + name, 10)
