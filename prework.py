f46af36b773fd21d57f6aeb5d8ba99d88336a7e3
zui <626255007@qq.com>
2016-11-22 22:19:36 +0800

    add cut_data, seperate_data

--- prework.py
+++ prework.py
@@ -4,6 +4,41 @@ import os
 FORMATLETTER = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_'
 
 
+# 数据分割
+def seperate_data(path, name, partnum=10):
+    """
+    names = ["BaseLine-BigData_1kUE_20ENB_UeAbnormal-Case_Group_1-Case_1",
+            "BaseLine-BigData_1kUE_20ENB_NORMAL-Case_Group_1-Case_1",
+            "BaseLine-BigData_1kUE_20ENB_paging-Case_Group_1-Case_1"]
+
+    path = "data/network_diagnosis_data/"
+    """
+    f = open(path + name + ".log", "rb")
+    con = f.readlines()
+
+    linelen = len(con)
+    batch_size = linelen / partnum
+
+    if not os.path.exists(path + name):
+        os.mkdir(path + name)
+    for idx in xrange(partnum - 1):
+        wf = open(path + name + "/part" + str(idx) + ".txt", "wb")
+        for line in con[idx * batch_size: (idx + 1) * batch_size]:
+            wf.write(line.strip())
+            wf.write("\n")
+        wf.close()
+
+    idx = partnum - 1
+    wf = open(path + name + "/part" + str(idx) + ".txt", "wb")
+    for line in con[idx * batch_size:]:
+        wf.write(line.strip())
+        wf.write("\n")
+    wf.close()
+
+    f.close()
+    print name + "  DONE!"
+
+
 # 单句去符号
 def format_string(string):
     for c in string:
@@ -13,56 +48,68 @@ def format_string(string):
     return retstring
 
 
-# log去符号(不分part)
-def get_text(filename):
-    f = open(filename + ".log", "rb")
-    context = f.readlines()
+# log去符号
+def get_text(filename, part=True, partnum=10):
+    if part:
+        for part in xrange(partnum):
+            f = open(filename + "/part" + str(part)+ ".txt", "rb")
+            context = f.readlines()
+            wf = open(filename + "/clean." + "part" + str(part) + ".txt", "wb")
+            for line in context:
+                s = format_string(line)
+                if s != "":
+                    wf.write(s + "\n")
+            wf.close()
+            f.close()
+            print filename + " part" + str(part) + " get_text_part  DONE!"
 
-    if not os.path.exists(filename):
-        os.mkdir(filename)
-
-    wf = open(filename + "/clean" + ".txt", "wb")
-    for line in context:
-        s = format_string(line)
-        if s != "":
-            wf.write(s + "\n")
-    wf.close()
-    f.close()
-    print filename + " get_text  DONE!"
-
-
-# log去符号(分part)
-def get_text_part(filename, partnum):
-    for part in xrange(partnum):
-        f = open(filename + "/part" + str(part)+ ".txt", "rb")
+    else:
+        f = open(filename + ".log", "rb")
         context = f.readlines()
-        wf = open(filename + "/clean." + "part" + str(part) + ".txt", "wb")
+
+        if not os.path.exists(filename):
+            os.mkdir(filename)
+
+        wf = open(filename + "/clean" + ".txt", "wb")
         for line in context:
             s = format_string(line)
             if s != "":
                 wf.write(s + "\n")
         wf.close()
         f.close()
-        print filename + " part" + str(part) + " get_text_part  DONE!"
-
-    print filename + " get_text_part  DONE!\n"
+    print filename + " get_text  DONE!"
 
 
-# 获得字典(不分part)
-def get_dic(filename):
+# 获得字典
+def get_dic(filename, part=True, partnum=10):
     wf = open(filename + "/dic.txt", "wb")
     dic = {}
 
-    f = open(filename + "/clean" + ".txt", "rb")
-    context = f.readlines()
-    for line in context:
-        sentence = line.strip().split(" ")
-        for word in sentence:
-            if word in dic:
-                dic[word] += 1
-            else:
-                dic[word] = 1
-    f.close()
+    if part:
+        for part in xrange(partnum):
+            f = open(filename + "/clean." + "part" + str(part) + ".txt", "rb")
+            context = f.readlines()
+            for line in context:
+                sentence = line.strip().split(" ")
+                for word in sentence:
+                    if word in dic:
+                        dic[word] += 1
+                    else:
+                        dic[word] = 1
+            f.close()
+            print filename + " part" + str(part) + " get_dic_part  DONE!"
+
+    else:
+        f = open(filename + "/clean" + ".txt", "rb")
+        context = f.readlines()
+        for line in context:
+            sentence = line.strip().split(" ")
+            for word in sentence:
+                if word in dic:
+                    dic[word] += 1
+                else:
+                    dic[word] = 1
+        f.close()
 
     okdic = sorted(dic.items(), key=lambda e: e[1], reverse=True)
     # print len(okdic)
@@ -73,35 +120,149 @@ def get_dic(filename):
     print filename + " get_dic  DONE!\n"
 
 
-# 获得字典(分part)
-def get_dic_part(filename, partnum):
-    wf = open(filename + "/dic.txt", "wb")
-    dic = {}
+# 样本分割
+def cut_data(path, name, label, part=True, partnum=10):
+    if part:
+        count = -1
+        for part in xrange(partnum):
+            f = open(path + name + "/clean.part" + str(part) + ".txt", "rb")
+            contxt = f.readlines()
 
-    for part in xrange(partnum):
-        f = open(filename + "/clean." + "part" + str(part) + ".txt", "rb")
-        context = f.readlines()
-        for line in context:
-            sentence = line.strip().split(" ")
-            for word in sentence:
-                if word in dic:
-                    dic[word] += 1
-                else:
-                    dic[word] = 1
-        f.close()
-        print filename + " part" + str(part) + " get_dic_part  DONE!"
+            num = []
+            for i, line in enumerate(contxt):
+                arr = line.strip().split()
+                if label in arr:
+                    num.append(i)
+            # print num, "\n"
+            nbegin = num[0]
+            nnew = [nbegin]
+            for n in num:
+                if n - nbegin > 10:
+                    print n - nbegin
+                    nnew.append(n)
+                    nbegin = n
 
-    okdic = sorted(dic.items(), key=lambda e: e[1], reverse=True)
-    #print len(okdic)
-    for item in okdic:
-        wf.write("%s,%d\n" % (item[0], item[1]))
+            newname = path + "cut/" + label + "."
+            i = 0
+            wwf = open(newname + str(i) + ".txt", "wb")
 
-    wf.close()
-    print filename + " get_dic_part  DONE!\n"
+            idx = 0
+            for i, line in enumerate(contxt):
+                if i - 10 in nnew:
+                    wwf.close()
+                    wwf = open(newname + str(idx) + ".txt", "wb")
+                    idx += 1
+                wwf.write(line)
+
+            print nnew, len(nnew)
+
+
+        newname = path + "cut/" + label + "."
+        wwf = open(newname + str(count) + ".txt", "wb")
+        for i, line in enumerate(contxt):
+            if i % 10000 == 0:
+                wwf.close()
+                count += 1
+                wwf = open(newname + str(count) + ".txt", "wb")
+            wwf.write(line)
+        print count
+
+    else:
+        f = open(path + name + "/clean.txt", "rb")
+        contxt = f.readlines()
+
+        num = []
+        for i, line in enumerate(contxt):
+            arr = line.strip().split()
+            if label in arr:
+                num.append(i)
+        # print num, "\n"
+
+        nbegin = num[0]
+        nnew = [nbegin]
+        for n in num:
+            if n - nbegin > 10:
+                print n - nbegin
+                nnew.append(n)
+                nbegin = n
+
+        newname = path + "cut/" + label + "."
+        i = 0
+        wwf = open(newname + str(i) + ".txt", "wb")
+
+        idx = 0
+        for i, line in enumerate(contxt):
+            if i - 10 in nnew:
+                wwf.close()
+                wwf = open(newname + str(idx) + ".txt", "wb")
+                idx += 1
+            wwf.write(line)
+
+        print nnew, len(nnew)
+
+
+# 直接切分(不使用log中的标签)
+def  cut_data_nolabel(path, name, label, part=True):
+    if part:
+        count = -1
+        for part in xrange(10):
+            f = open(path + name + "/clean.part" + str(part) + ".txt", "rb")
+            contxt = f.readlines()
+
+            newname = path + "cut/" + label + "."
+            wwf = open(newname + str(count) + ".txt", "wb")
+            for i, line in enumerate(contxt):
+                if i % 10000 == 0:
+                    wwf.close()
+                    count += 1
+                    wwf = open(newname + str(count) + ".txt", "wb")
+                wwf.write(line)
+            print count
+
+    else:
+        f = open(path + name + "/clean.txt", "rb")
+        contxt = f.readlines()
+
+        num = []
+        for i, line in enumerate(contxt):
+            arr = line.strip().split()
+            if label in arr:
+                num.append(i)
+        # print num, "\n"
+
+        nbegin = num[0]
+        nnew = [nbegin]
+        for n in num:
+            if n - nbegin > 10:
+                print n - nbegin
+                nnew.append(n)
+                nbegin = n
+
+        newname = path + "cut/" + label + "."
+        i = 0
+        wwf = open(newname + str(i) + ".txt", "wb")
+
+        idx = 0
+        for i, line in enumerate(contxt):
+            if i - 10 in nnew:
+                wwf.close()
+                wwf = open(newname + str(idx) + ".txt", "wb")
+                idx += 1
+            wwf.write(line)
+
+        print nnew, len(nnew)
+
 
 
 if __name__ == "__main__":
     path = "data/network_diagnosis_data/"
+
+    # path = "data/network_diagnosis_data/"
+    # name = "BaseLine-BigData_1kUE_20ENB_UeAbnormal-Case_Group_1-Case_1_new_With_Tag"
+    # seperate_data(path, name)
+    # cut_data("data/network_diagnosis_data/", "BaseLine-BigData_1kUE_20ENB_gtpcbreakdown-Case_Group_1-Case_1", "GTPC_TUNNEL_PATH_BROKEN")
+    # cut_data("data/network_diagnosis_data/", "BaseLine-BigData_1kUE_20ENB_NORMAL-Case_Group_1-Case_1", "NORMAL", True)
+    # cut_data("data/network_diagnosis_data/", "BaseLine-BigData_1kUE_20ENB_NORMAL-Case_Group_1-Case_1", "NORMAL", True)
 
     #names = ["BaseLine-BigData_1kUE_20ENB_UeAbnormal-Case_Group_1-Case_1",
     #         "BaseLine-BigData_1kUE_20ENB_NORMAL-Case_Group_1-Case_1",
@@ -110,5 +271,7 @@ if __name__ == "__main__":
     #for name in names:
     #    get_text_part(path + name, 10)
     #    get_dic_part(path + name, 10)
-    get_text(path + "BaseLine-BigData_1kUE_20ENB_gtpcbreakdown-Case_Group_1-Case_1")
-    get_dic(path + "BaseLine-BigData_1kUE_20ENB_gtpcbreakdown-Case_Group_1-Case_1")
+    name = "BaseLine-BigData_1kUE_20ENB_UeAbnormal-Case_Group_1-Case_1_new_With_Tag"
+    get_text(path + name, part=False)
+    get_dic(path + name, part=False)
+    cut_data(path + name, )
