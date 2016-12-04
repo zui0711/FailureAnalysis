88d9b9c442289edae3bcb87af8f9497ac005f4ab
zui <626255007@qq.com>
2016-12-04 16:33:16 +0800

    local setting

--- setting.py
+++ setting.py
@@ -6,11 +6,22 @@ m_hidden_dim = 72
 
 m_sent_len = 20
 m_word_dim = 50
+m_batch_size = 10
 
-m_path = "data/network_diagnosis_data/cut1000/"
-#m_path =
+#m_path = "data/network_diagnosis_data/cut1000/"
+m_path = "/media/zui/work/NETWORK/network_diagnosis_data/"
 m_model_w2v_name = "word_emb_size50wind100count100iter30"
 m_epoch = 5
+
+m_labels = ["NORMAL",
+            "GTPC_TUNNEL_PATH_BROKEN",
+            "Paging",
+            "UeAbnormal"]
+
+m_names = ["BaseLine-BigData_1kUE_20ENB_NORMAL-Case_Group_1-Case_1",
+           "BaseLine-BigData_1kUE_20ENB_gtpcbreakdown-Case_Group_1-Case_1",
+           "BaseLine-BigData_1kUE_20ENB_paging-Case_Group_1-Case_1",
+           "BaseLine-BigData_1kUE_20ENB_UeAbnormal-Case_Group_1-Case_1"]
 #model_w2v = Word2Vec.load(path + "word_emb_size50wind100count100iter30")
 
 #print model_w2v
